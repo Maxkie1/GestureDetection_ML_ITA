697245ffde119399c634a56afb9597667cc3360a
Max Kiefer <inf20111@lehre.dhwb-stuttgart.de>
2022-12-08 22:46:54 +0100

    add preprocess_data and main

---
+++ src/main.py
@@ -0,0 +1,77 @@
+"""Uses a trained model to classify the gestures performed by the detected hands in a video stream.
+
+To use the model, run the script as follows:
+    python main.py
+
+The video stream from the default webcam will be displayed, and the gestures performed by the detected hands
+will be classified and displayed on the video.
+
+The script assumes that the trained model is saved in a file named gesture_recognition_model.h5.
+"""
+
+# Import the necessary modules
+import cv2
+import mediapipe as mp
+
+# Import the drawing utilities and styles from the MediaPipe Hands module
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+# Print the docstring
+print(__doc__)
+
+# Create a VideoCapture object to access the webcam
+cap = cv2.VideoCapture(0)
+
+# Use the Hands class from the MediaPipe Hands module to process the video frames
+with mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+    
+    # Loop through the frames of the video
+    while cap.isOpened():
+    
+        # Read a frame from the video
+        success, image = cap.read()
+        
+        # If the frame is empty, skip it and print a message
+        if not success:
+          print("Ignoring empty camera frame.")
+          continue
+        
+        # Make the image not writeable to improve performance
+        image.flags.writeable = False
+        # Convert the image to RGB color space
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # Use the Hands object to process the image and detect hand landmarks
+        results = hands.process(image)
+        
+        # Make the image writeable again and convert it to BGR color space
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        # If hand landmarks were detected, draw them on the image
+        if results.multi_hand_landmarks:
+
+            # Iterate over the detected hand landmarks
+            for hand_landmarks in results.multi_hand_landmarks:
+                # Draw the detected hand landmarks on the image
+                mp_drawing.draw_landmarks(
+                    image,
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
+
+        # Flip the image horizontally for a selfie-view display
+        cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+        
+        # Check for user input to exit the program
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+              
+# Release the capture and destroy the display window
+cap.release()
+cv2.destroyAllWindows()
---
+++ src/preprocess_data.py
@@ -0,0 +1,98 @@
+
+
+
+# Import the necessary modules
+import cv2
+import mediapipe as mp
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Import the drawing utilities and styles from the MediaPipe Hands module
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+# Print the docstring
+print(__doc__)
+
+# For static images:
+IMAGE_FILES = ['../data/1.jpg', '../data/2.jpg', '../data/3.jpg']
+
+# Use the Hands class from the MediaPipe Hands module to process the static images
+with mp_hands.Hands(
+    static_image_mode=True,
+    max_num_hands=2,
+    min_detection_confidence=0.5) as hands:
+
+    # Iterate over the list of image files
+    for idx, file in enumerate(IMAGE_FILES):
+    
+        # Read an image, flip it around y-axis for correct handedness output
+        image = cv2.flip(cv2.imread(file), 1)
+
+        # Convert the BGR image to RGB before processing
+        results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+        # Print the handedness of the detected hands and draw hand landmarks on the image
+        print('Handedness:', results.multi_handedness)
+
+        # If no hand landmarks are detected, skip to the next iteration
+        if not results.multi_hand_landmarks:
+            continue
+
+        # Get the height and width of the image
+        image_height, image_width, _ = image.shape
+        print('Image shape:', image.shape)
+
+        # Iterate over the detected hand landmarks
+        for hand_landmarks in results.multi_hand_landmarks:
+
+            # Convert the landmarks to a NumPy array
+            coordinates = np.array([[lmk.x, lmk.y, lmk.z] for lmk in hand_landmarks.landmark])
+
+            # Get the wrist landmark as the origin for the relative coordinates
+            wrist = np.array([hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x, hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y, hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].z])
+
+            # Calculate the relative coordinates
+            relative_coordinates = np.subtract(coordinates, wrist)
+
+            # Normalize the relative coordinates
+            normalized_coordinates = (relative_coordinates - np.min(relative_coordinates)) / np.ptp(relative_coordinates)
+
+            # Flatten the normalized coordinates
+            flattened_coordinates = normalized_coordinates.flatten()
+
+            # Print the coordinates and relative coordinates
+            print('coordinates:', coordinates)
+            print('relative_coordinates:', relative_coordinates)
+            print('normalized_coordinates:', normalized_coordinates)
+            print('flattened_coordinates:', flattened_coordinates)
+            print('flattened_coordinates.shape:', flattened_coordinates.shape)
+
+        # Plot the relative coordinates
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+        # Plot the points
+        ax.scatter(normalized_coordinates[:, 0], normalized_coordinates[:, 1], normalized_coordinates[:, 2])
+         # Thumb plot
+        thumb_coordinates = normalized_coordinates.take([0,1,2,3,4], axis=0)
+        ax.plot(thumb_coordinates[:, 0], thumb_coordinates[:, 1], thumb_coordinates[:, 2], color='blue')
+        # Index finger plot
+        index_finger_coordinates = normalized_coordinates.take([0,5,6,7,8], axis=0)
+        ax.plot(index_finger_coordinates[:, 0], index_finger_coordinates[:, 1], index_finger_coordinates[:, 2], color='blue')
+        # Middle finger plot
+        middle_finger_coordinates = normalized_coordinates.take([9,10,11,12], axis=0)
+        ax.plot(middle_finger_coordinates[:, 0], middle_finger_coordinates[:, 1], middle_finger_coordinates[:, 2], color='blue')
+        # Ring finger plot
+        ring_finger_coordinates = normalized_coordinates.take([13,14,15,16], axis=0)
+        ax.plot(ring_finger_coordinates[:, 0], ring_finger_coordinates[:, 1], ring_finger_coordinates[:, 2], color='blue')
+        # Pinky plot
+        pinky_coordinates = normalized_coordinates.take([0,17,18,19,20], axis=0)
+        ax.plot(pinky_coordinates[:, 0], pinky_coordinates[:, 1], pinky_coordinates[:, 2], color='blue')
+        # Finger connection plot
+        finger_connection_coordinates = normalized_coordinates.take([5,9,13,17], axis=0)
+        ax.plot(finger_connection_coordinates[:, 0], finger_connection_coordinates[:, 1], finger_connection_coordinates[:, 2], color='blue')
+        # Set the title
+        ax.set_title('Relative Coordinates Plot')
+        # Show the plot
+        plt.show()
