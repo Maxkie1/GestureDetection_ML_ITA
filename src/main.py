606bb00449c4df64ed3074c27541eae23d130ec4
Max Kiefer <inf20111@lehre.dhwb-stuttgart.de>
2022-12-11 21:27:44 +0100

    add live prediction feed

--- src/main.py
+++ src/main.py
@@ -7,7 +7,10 @@ The script assumes that the trained model is saved in a file named gesture_recog
 
 # Import the necessary modules
 import cv2
+import tensorflow as tf
+import numpy as np
 import mediapipe as mp
+
 
 # Import the drawing utilities and styles from the MediaPipe Hands module
 mp_drawing = mp.solutions.drawing_utils
@@ -19,6 +22,32 @@ print(__doc__)
 
 # Create a VideoCapture object to access the webcam
 cap = cv2.VideoCapture(0)
+
+# Load the trained model
+model = tf.keras.models.load_model("../models/gesture_recognition_model.h5")
+
+# Predict the gesture performed by the detected hands in the video stream
+def predict_gesture(hand_landmarks):
+
+    # Convert the landmarks to a NumPy array
+    coordinates = np.array([[lmk.x, lmk.y] for lmk in hand_landmarks.landmark])
+    # Get the wrist landmark as the origin for the relative coordinates
+    wrist = np.array([hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x, hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y])
+    # Calculate the relative coordinates and flip the hand
+    relative_coordinates = -(coordinates - wrist)
+    # Normalize the relative coordinates
+    normalized_coordinates = (relative_coordinates - np.min(relative_coordinates)) / np.ptp(relative_coordinates)
+    # Flatten the normalized coordinates
+    flattened_coordinates = normalized_coordinates.flatten()
+
+    # Predict the gesture performed by the hand
+    prediction = model.predict(flattened_coordinates.reshape(1, -1))
+    # Get the index of the predicted gesture
+    predicted_gesture = np.argmax(prediction)
+    # Get the confidence of the prediction
+    confidence = prediction[0][predicted_gesture]
+    # Print the gesture and confidence
+    print("Predicted gesture: {}, confidence: {}".format(predicted_gesture, confidence))
 
 # Use the Hands class from the MediaPipe Hands module to process the video frames
 with mp_hands.Hands(
@@ -61,6 +90,9 @@ with mp_hands.Hands(
                     mp_drawing_styles.get_default_hand_landmarks_style(),
                     mp_drawing_styles.get_default_hand_connections_style())
 
+                # Predict the gesture performed by the hand
+                predict_gesture(hand_landmarks)
+                
         # Flip the image horizontally for a selfie-view display
         cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
         
