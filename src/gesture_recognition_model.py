c421dab4d8023a1e030a7044b057696f90966e39
Max Kiefer <inf20111@lehre.dhwb-stuttgart.de>
2022-12-11 13:47:23 +0100

    update load_data

--- src/gesture_recognition_model.py
+++ src/gesture_recognition_model.py
@@ -23,10 +23,13 @@ def load_data(h5_path):
 
     # Load the data and labels from the HDF5 file
     for dataset_name in h5_file['hand_landmarks_group']:
-        # Load the data and labels from the dataset
+
+        # Print the dataset name, label, and shape
         print('Dataset loaded: ', dataset_name)
         print('Dataset label:', h5_file['hand_landmarks_group'][dataset_name].attrs['label'])
         print('Dataset shape:', h5_file['hand_landmarks_group'][dataset_name].shape)
+
+        # Append the data to the list of data
         x = np.append(x, h5_file['hand_landmarks_group'][dataset_name], axis=0)
 
         # Append the label to the list of labels
